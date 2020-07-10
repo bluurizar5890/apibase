13dfa4ed17483debe575d274c715444e20f8a17b
Desa Home <blu.urizar@gmail.com>
2020-07-09 18:52:00 -0600

    Primer commit api base

---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    api: {
+      port: process.env.API_PORT || 3000,
+      bitacora_cambios:process.env.BITACORA_CAMBIOS || true
+    },
+    jwt: {
+      secret: process.env.JWT_SECRET || "Yahk9Lev5lBdANo6UPQuIgCVHinXsm8M",
+    },
+    bd: {
+      database: "demobase",
+      username: "root",
+      password: "Blopez$1991",
+      host: "127.0.0.1",
+      dialect: "mysql",
+      port:3307,
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+      },
+    },
+  };
+  
