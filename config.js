8c5f60b44a48ac33bf1cda8c67bec5ee3fc2e46e
Desa Home <blu.urizar@gmail.com>
2020-07-10 07:00:16 -0600

    Se cambio base de datos a digital ocena

--- config.js
+++ config.js
@@ -1,24 +1,23 @@
 module.exports = {
-    api: {
-      port: process.env.API_PORT || 3000,
-      bitacora_cambios:process.env.BITACORA_CAMBIOS || true
+  api: {
+    port: process.env.API_PORT || 3000,
+    bitacora_cambios: process.env.BITACORA_CAMBIOS || true
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET || "Yahk9Lev5lBdANo6UPQuIgCVHinXsm8M",
+  },
+  bd: {
+    database: "demobase",
+    username: "doadmin",
+    password: "k9tu3z285cuemoez",
+    host: "desarrollo-do-user-7602079-0.a.db.ondigitalocean.com",
+    dialect: "mysql",
+    port: 25060,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
     },
-    jwt: {
-      secret: process.env.JWT_SECRET || "Yahk9Lev5lBdANo6UPQuIgCVHinXsm8M",
-    },
-    bd: {
-      database: "demobase",
-      username: "root",
-      password: "Blopez$1991",
-      host: "34.94.42.173",
-      dialect: "mysql",
-      port:3306,
-      pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-      },
-    },
-  };
-  
+  },
+};
